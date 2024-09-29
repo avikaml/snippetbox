6f016a1ebb2b653f6b184f9faca05d6cefa66684
avikaml <avikaml@post.bgu.ac.il>
2024-09-29 12:05:02 +0300

    End to end testing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # snippetbox
 
- Currently - page 342 - 14.2
+ Currently - page 347 - 14.3
 
  // Do a proper readme and figure out how to make it fancy! use - readme.so!!
 
--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,34 +1,21 @@
 package main
 
 import(
-	"bytes"
-	"io"
+
 	"net/http"
-	"net/http/httptest"
+
 	"testing"
 	"github.com/avikaml/snippetbox/internal/assert"
 )
 
 func TestPing(t *testing.T){
-	rr := httptest.NewRecorder()
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil{
-		t.Fatal(err)
-	}
+	app := newTestApplication(t)
 
-	ping(rr, r)
-	rs := rr.Result()
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	defer rs.Body.Close()	
-	body, err := io.ReadAll(rs.Body)
-
-	if err!= nil {
-		t.Fatal(err)
-	}
-
-	bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	code, _, body := ts.get(t, "/ping")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
